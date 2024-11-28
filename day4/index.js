2806c9dd8bdd7cab13dc89ce1018f54f329dd81a
louis <louismoses.rait@gmail.com>
2024-11-28 20:01:52 +0800

    lesson: app.get .post routing

--- day4/index.js
+++ day4/index.js
@@ -5,10 +5,30 @@ const port = 3000;
 
 // console.dir(app);
 
-app.use((req, res) => {
-    console.log("we got new request");
-    // console.log(req);
-    res.send("Hello World!");
+// app.use((req, res) => {
+//     console.log("we got new request");
+//     // console.log(req);
+//     res.send("Hello World!");
+// });
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/cats", (req, res) => {
+    res.send("meow");
+});
+
+app.get("/dogs", (req, res) => {
+    res.send("aw aw");
+});
+
+app.post("/dogs", (req, res) => {
+    res.send("grrrrrr");
+});
+
+app.get("*", (req, res) => {
+    res.send("404 | Page not found");
 });
 
 app.listen(port, () => {
