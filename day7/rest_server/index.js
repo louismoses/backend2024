3e1c32f5c8803542b005df924cf538705396ad03
louis <louismoses.rait@gmail.com>
2024-12-03 19:53:00 +0800

    develop: shawarma order api

--- day7/rest_server/index.js
+++ day7/rest_server/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/shawarma", (req, res) => {
     res.send("Get Shawarma Sarap");
 });
 
 app.post("/shawarma", (req, res) => {
-    res.send("Post method Shawarma Sarap");
+    const { flavor, orderCount } = req.body;
+    console.log(req.body);
+    res.send(`${orderCount} ${flavor} Shawarma Sarap coming up!`);
 });
 
 app.listen(port, () => {
